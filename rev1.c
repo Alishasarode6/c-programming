04ecf1efc7c70d5b95292446ec2222abc8df37f1
Savita Sarode <sarodesavita64@gmail.com>
2025-06-27 15:55:27 +0530

    rev num

---
+++ rev1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+  int n,temp,rev=0,rem;
+  printf("enter a number");
+  scanf("%d", &n);
+
+  temp=n;
+  while(n>0)
+  {
+    int rem = n%10;
+    rev = rev*10+rem;
+    n=n/10;
+
+  }
+if(rev == temp)
+  printf("palindrome number");
+else
+   printf("not palindrome number");
+
+   return 0;
+}
