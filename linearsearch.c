30f17e3d6c61ca271cf5f86243fe71541c620ddb
Savita Sarode <sarodesavita64@gmail.com>
2025-07-01 15:15:57 +0530

    linear

--- linearsearch.c
+++ linearsearch.c
@@ -1,26 +1,30 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int a[100],n,i,key,flag;
-    printf("enter element for search");
-    scanf("%d",&key);
-    flag=0;
+    int n, i, a[50], max, flag, key;
+    printf("Enter count of number :");
+    scanf("%d", &n);
+    printf("enter data of count %d :",n);
+    for (i = 0; i < n; i++)
+     scanf("%d",&a[i]);
 
-    for(i=0;i<=n;i++)
+    printf("\n now enter element to search :");
+    scanf("%d", &key);
+
+    flag = 0;
+
+    for (i = 0; i < n; i++)
     {
-        if(key==a[i])
+        if (key == a[i])
         {
-            flag=1;
+            flag = 1;
             break;
         }
     }
+    if (flag == 1)
+        printf("found");
+    else
+        printf("not found");
 
-      if(flag==1)
-           printf("found");
-        
-        else
-            printf("not found");
-        
         return 0;
-    
 }
