29ac432f622b067627ca5f1ea68f2b4fa4ee2337
Savita Sarode <sarodesavita64@gmail.com>
2025-07-08 13:58:11 +0530

    pointer

---
+++ pointer3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void swap(int a,int b)
+{
+    int temp=a;
+    a=b;
+    b=temp;
+
+}
+int main()
+{
+    int x,y;
+    printf("enter 2 numbers :");
+    scanf("%d%d",&x,&y);
+    swap(x,y);
+    printf("after swapping \nx=%d\ny=%d",x,y);
+
+    return 0;
+}
